4284c7634995a5205f861b005445e342e30c249e
Edwar León <edwar.leon.m@gmail.com>
2023-08-07 16:09:13 -0500

    se mejora la interfaz del darkmode

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -24,9 +24,10 @@ const DarkMode = () => {
   }, [darkMode]);
 
   return (
-    <div onClick={handleDarkMode} className="">
+    <div onClick={handleDarkMode} className="bg-white/50 dark:bg-black/50 px-1 rounded-lg py-1 flex justify-center items-center">
       {/* <div className="h-6 w-6 bg-slate-950 rounded-full cursor-pointer hover:translate-x-4 active:translate-x-4"></div> */}
-      <i className="bx bxs-brightness-half text-3xl text-cyan-500 transition-all duration-500 cursor-pointer hover:rotate-180 hover:text-yellow-400  dark:text-yellow-400 dark:hover:text-cyan-500"></i>
+      <i className="bx bxs-brightness-half text-3xl text-slate-700 transition-all duration-500 cursor-pointer hover:rotate-180 hover:text-slate-950 dark:text-yellow-400 dark:hover:text-amber-500 "></i>
+
     </div>
   );
 };
